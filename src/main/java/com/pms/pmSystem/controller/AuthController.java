ce22a9f1f20b5b09cb6a4051f04dd7b861c89923
jprayco <praycojean25@gmail.com>
2025-12-23 16:23:08 +0800

    add field validation

--- src/main/java/com/pms/pmSystem/controller/AuthController.java
+++ src/main/java/com/pms/pmSystem/controller/AuthController.java
@@ -1,6 +1,13 @@
 package com.pms.pmSystem.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,21 +15,55 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pms.pmSystem.model.Users;
 import com.pms.pmSystem.service.authentication.UserService;
 
+import jakarta.validation.Valid;
 
 @RestController
 public class AuthController {
+
     @Autowired
     UserService service;
 
     @PostMapping("/register")
-    public Users register(@RequestBody Users users){
-        return service.register(users);
+    public ResponseEntity<?> register(@Valid @RequestBody Users users) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("statusCode", HttpStatus.OK.value());
+        response.put("data", service.register(users));
+
+        return ResponseEntity.ok(response);
     }
 
+
     @PostMapping("/login")
-    public String login(@RequestBody Users users) {
-        System.out.println(users);
-        return service.verify(users);
+    public ResponseEntity<Map<String, Object>> login(@RequestBody Users users) {
+        String token = service.verify(users);
+        
+        Map<String, Object> response = new HashMap<>();
+        Map<String, String> data = new HashMap<>();
+        
+        data.put("token", token);
+        response.put("statusCode", HttpStatus.OK.value());
+        response.put("data", data);
+        
+        return ResponseEntity.ok(response);
     }
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        Map<String, String> fieldErrors = new HashMap<>();
+        
+        ex.getBindingResult().getFieldErrors().forEach(error -> 
+            fieldErrors.put(error.getField(), error.getDefaultMessage())
+        );
+        
+        errors.put("timestamp", System.currentTimeMillis());
+        errors.put("status", HttpStatus.BAD_REQUEST.value());
+        errors.put("error", "Validation Failed");
+        errors.put("message", "One or more fields failed validation");
+        errors.put("fieldErrors", fieldErrors);
+        
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
--- src/main/java/com/pms/pmSystem/model/Users.java
+++ src/main/java/com/pms/pmSystem/model/Users.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +18,25 @@ public class Users {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
-    private String lname, fname;
+    @NotBlank(message = "Last name is required")
+    private String lname;
+    
+    @NotBlank(message = "First name is required")
+    private String fname;
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+    
     private int status;
     private String phone;
     private String role;
+    
+    @NotBlank(message = "Username is required")
     private String username;
+    
+    @NotBlank(message = "Password is required")
     private String password;
-
     public Users(String username, String password) {
         this.username = username;
         this.password = password;
